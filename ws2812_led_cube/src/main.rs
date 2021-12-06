7ba3f8642f3fd7da85368900aa1bb15ca8b28851
Weird Constructor <weirdconstructor@gmail.com>
2021-12-06 05:05:50 +0100

    added the ADC for light resistor

--- ws2812_led_cube/src/main.rs
+++ ws2812_led_cube/src/main.rs
@@ -8,10 +8,12 @@ use embedded_time::duration::*;
 use embedded_hal::timer::CountDown;
 use panic_probe as _;
 
+use embedded_hal::adc::OneShot;
 use pico::hal::{
     pac,
     clocks::{Clock, init_clocks_and_plls},
     sio::Sio,
+    adc::Adc,
     watchdog::Watchdog,
     timer::Timer,
 };
@@ -24,6 +26,30 @@ use ws2812_pio::Ws2812;
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+struct MovingAvg {
+    buf:    [u16; 30],
+    ptr:    u16,
+}
+
+impl MovingAvg {
+    pub fn new() -> Self {
+        Self {
+            buf: [0xFFFF; 30],
+            ptr: 0,
+        }
+    }
+
+    pub fn next(&mut self, input: u16) -> u16 {
+        self.buf[self.ptr as usize] = input;
+        self.ptr = ((self.ptr as usize + 1) % self.buf.len()) as u16;
+        let mut sum : usize = 0;
+        for b in &self.buf {
+            sum += *b as usize;
+        }
+        (sum / self.buf.len()) as u16
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -66,6 +92,9 @@ fn main() -> ! {
         timer.count_down(),
     );
 
+    let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
+    let mut adc_pin_0 = pins.gpio26.into_floating_input();
+
 
     let mut n: u8 = 0;
     let mut leds : [RGB8; 7] = [
@@ -78,16 +107,38 @@ fn main() -> ! {
         (255, 0, 0).into(),
     ];
 
+    let mut avg = MovingAvg::new();
+
+    let mut timer_cnt : usize = 0;
+    let mut state_on = false;
+
+    let mut r : u16 = avg.next(adc.read(&mut adc_pin_0).unwrap());
     loop {
+        timer_cnt = timer_cnt.wrapping_add(1);
         n = n.wrapping_add(1);
-        let b = if n > 128 { 128 - (n - 128) } else { n };
 
         if n == 255 {
             rot(&mut leds[2..]);
         }
 
-        ws.write(brightness(leds.iter().copied(), 64 + (b / 3))).unwrap();
-        delay.start(7.milliseconds());
+        if n % 8 == 0 {
+            r = avg.next(adc.read(&mut adc_pin_0).unwrap());
+            info!("adc={}", r);
+        }
+
+        if state_on && timer_cnt > (10000 / 16) {
+            state_on = false;
+
+        } else if r <= 1000 {
+            state_on = true;
+            timer_cnt = 0;
+        }
+
+        let b = if n > 128 { 128 - (n - 128) } else { n };
+        let vbrightness = if !state_on { 0 } else { 64 + (b / 3) };
+
+        ws.write(brightness(leds.iter().copied(), vbrightness)).unwrap();
+        delay.start(16.milliseconds());
         let _ = nb::block!(delay.wait());
     }
 }
